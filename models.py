af5077925aac9f5623956835cbf0fbdbf5d1bd85
HermonMulat <hermonmulat@gmail.com>
2018-04-07 22:35:46 -0400

    Add various ensemble models for text analysis

---
+++ models.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import SVR
+from vectorize_data import get_data
+from sklearn.linear_model import LinearRegression as LR
+from sklearn.ensemble import AdaBoostRegressor as ADA
+from sklearn.ensemble import RandomForestRegressor as RF
+from sklearn.ensemble import GradientBoostingRegressor as GB
+from sklearn.ensemble import BaggingRegressor as BAG
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+
+def run_model():
+    x_train, x_test, y_train, y_test = get_data(None)
+    clf = BAG()
+    clf.fit(x_train, y_train)
+    print clf.score(x_test, y_test)
+
+if __name__ == "__main__":
+    run_model()
--- vectorize_data.py
+++ vectorize_data.py
@@ -8,55 +8,29 @@ def read_data(fn = "./data/opoint.csv"):
     return df
 
 def split_data(df):
-    x = df.iloc[:,:-1]
     y = df['momentum']
-
+    x = df.drop('momentum', axis = 1)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
     return x_train, x_test, y_train, y_test
 
-def vectorize(x_train, x_test):
+def vectorize_text(x_train, x_test, feature_count):
+    count_vect = CountVectorizer(strip_accents='unicode', analyzer = 'word', 
+                                 max_features = feature_count)
+    text_train = count_vect.fit_transform(x_train).toarray()
+    text_test = count_vect.transform(x_test).toarray()
+    text_train,text_test = pd.DataFrame(text_train),pd.DataFrame(text_test)
+    
+    return text_train, text_test
 
-    count_vect = CountVectorizer()
-    tf = TF()
-    header_text_train = tf.fit_transform(count_vect.fit_transform(x_train["header_text"])).toarray()
-    header_text_test = tf.transform(count_vect.transform(x_test["header_text"])).toarray()
-    header_text_train,header_text_test = pd.DataFrame(header_text_train), pd.DataFrame(header_text_test)
-
-    count_vect = CountVectorizer()
-    tf = TF()
-    summary_text_train = tf.fit_transform(count_vect.fit_transform(x_train["summary_text"])).toarray()
-    summary_text_test = tf.transform(count_vect.transform(x_test["summary_text"])).toarray()
-    summary_text_train,summary_text_test = pd.DataFrame(summary_text_train),pd.DataFrame(summary_text_test)
-
-    count_vect = CountVectorizer()
-    tf = TF()
-    body_text_train = tf.fit_transform(count_vect.fit_transform(x_train["body_text"])).toarray()
-    body_text_test = tf.transform(count_vect.transform(x_test["body_text"])).toarray()
-    body_text_train,body_text_test = pd.DataFrame(body_text_train),pd.DataFrame(body_text_test)
-
-    x_train = header_text_train.\
-             join(summary_text_train,lsuffix='_head', rsuffix='_summary').\
-             join(body_text_train,lsuffix='_headsum',rsuffix='_body')
-             #.\join(x_train[["firstSource_name"]],lsuffix='_text',rsuffix='_source')
-
-    x_test = header_text_test.\
-             join(summary_text_test,lsuffix='_head', rsuffix='_summary').\
-             join(body_text_test,lsuffix='_headsum',rsuffix='_body')
-             #.\join(x_test[["firstSource_name"]],lsuffix='_text',rsuffix='_source')
-
-    print x_train.shape
-    print x_test.shape
-    return summary_text_train, summary_text_test
-
-def master_function():
+def get_data(feature_count = 10000):
     df = read_data()
     df = df[df.momentum.notnull()]
     df = df.fillna("")
     x_train, x_test, y_train, y_test = split_data(df)
-    x_train, x_test = vectorize(x_train, x_test)
+    x_train, x_test = vectorize_text(x_train['header_text'], 
+                                     x_test['header_text'],feature_count)
 
     return x_train, x_test, y_train, y_test
-
 
 if __name__ == "__main__":
     master_function()
